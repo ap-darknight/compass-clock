09c939305b17e3e47df24b4103aee87a5ce90bfd
Ashutosh Pandey <pandeyashutosh150799@gmail.com>
2020-06-15 07:28:08 +0530

    Updates

---
+++ precache-manifest.4133ca8ee21c5c2a16742334484e08e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d63f352663d3561072ef220ae832e6",
+    "url": "/compass-clock/index.html"
+  },
+  {
+    "revision": "a508478a20de7de23525",
+    "url": "/compass-clock/static/css/main.854fe1cd.chunk.css"
+  },
+  {
+    "revision": "13e96fe52287c7786fdf",
+    "url": "/compass-clock/static/js/2.2b0cc52a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/compass-clock/static/js/2.2b0cc52a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a508478a20de7de23525",
+    "url": "/compass-clock/static/js/main.3efe2c46.chunk.js"
+  },
+  {
+    "revision": "cef3fc184a0f786e9be7",
+    "url": "/compass-clock/static/js/runtime-main.a0d9f6bb.js"
+  }
+]);
